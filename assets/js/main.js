78c60a2eb2cc36a1af1bb017e734ba2b9866c4eb
EliasFredoni <fredoni.elias@gmail.com>
2022-04-14 13:15:33 +0200

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.getElementById("info").innerHTML;
+
+info.innerHTML = "<h1>Hello World</h1>";
+info.innerHTML += "<h2>How are you?</h2>";
+
+document.getElementById("container").innerHTML;
+
+container.innerHTML = "<p>start of the element</P>"
+
+document.write("<p>end of the element</p>");
